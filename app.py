f35ac2136d2451b06b60767105d25d9a462ca195
zuoxiaolei <1905561110@qq.com>
2023-07-30 00:30:25 +0800

    fix qtrade3

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
 
 is_local = False
 ttl = 3600
@@ -31,7 +33,21 @@ with col1:
     code1 = st.text_input('股代码/ETF基金代码', '159915')
     select_stock_df = stock_df[stock_df.code == code1]
     st.dataframe(select_stock_df, height=height, hide_index=True)
+    if len(select_stock_df) > 0:
+        x = [datetime.datetime.strptime(ele, '%Y-%m-%d') for ele in select_stock_df.date]
+        y = select_stock_df['slope_standard']
+        plt.plot(x, y)
+        plt.title(code1)
+        st.pyplot(plt.gcf())
+
 with col2:
     code2 = st.text_input('支付宝基金代码', '162605')
     select_fund_df = fund_df[fund_df.code == code2]
     st.dataframe(select_fund_df, height=height, hide_index=True)
+    if len(select_fund_df) > 0:
+        x = [datetime.datetime.strptime(ele, '%Y-%m-%d') for ele in select_fund_df.date]
+        y = select_fund_df['slope_standard']
+        plt.figure()
+        plt.plot(x, y)
+        plt.title(code2)
+        st.pyplot(plt.gcf())
